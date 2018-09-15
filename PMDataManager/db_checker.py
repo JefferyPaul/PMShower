d56c2d84ddb44c1dfbaa747f33251e3a110fbd09
JefferyPaul <595837423@qq.com>
2018-09-16 00:26:35 +0800

    框架

---
+++ PMDataManager/db_checker.py
@@ -0,0 +1,33 @@
+from datetime import *
+
+
+def check_traders(strategy_id=None):
+	pass
+
+
+def check_strategy_type():
+	pass
+
+
+def check_strategies(strategy_type=None):
+	pass
+
+
+def check_product(create_user=None):
+	pass
+
+
+def check_product_portfolio_weight(product_id=None):
+	pass
+
+
+def check_strategy_portfolio(user_id=None):
+	pass
+
+
+def check_strategy_portfolio_weight(strategy_portfolio=None):
+	pass
+
+
+if __name__ == '__main__':
+	pass
--- Shower/PMShower.py
+++ Shower/PMShower.py
@@ -1,27 +1,31 @@
 from Shower.PMItem import PMItem
 from datetime import *
 from PMDataManager.data_collector import *
+from PMDataManager import db_checker
 
 
 class PMShower:
-	def __init__(self,
-	             start_date=None,
-	             end_date=None):
+	def __init__(self, start_date=None, end_date=None):
+		def set_date(ser_type):
+			if ser_type == 'start':
+				return datetime(year=2015, month=1, day=1)
+			else:
+				return datetime.today()
+
 		self.items = []
 		if start_date is None:
-			self.start_date = self.set_date("start")
+			self.start_date = set_date("start")
 		else:
 			self.start_date = start_date
 		if end_date is None:
-			self.end_date = self.set_date("end")
+			self.end_date = set_date("end")
 		else:
 			self.end_date = end_date
 
-	def set_date(self, ser_type):
-		if ser_type == 'start':
-			return datetime(year=2015, month=1, day=1)
-		else:
-			return datetime.today()
+	def select_item(self):
+		item = db_checker
+		self.add_item(item)
+		pass
 
 	def add_item(self):
 		item = PMItem(
@@ -29,6 +33,9 @@ class PMShower:
 		)
 		self.items.append(item)
 
-	def show(self):
+	def collect_data(self):
+		pass
+
+	def show(self, set_mdd=None, set_volatility=None):
 		data = data_collector(self.items)
 		pass
