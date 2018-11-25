1c7dbcf6274642c22e28ffc9ff8cce87e3a0e33b
JefferyPaul <595837423@qq.com>
2018-11-25 20:47:23 +0800

    框架

--- PMDataManager/PMData.py
+++ PMDataManager/PMData.py
@@ -1,17 +1,18 @@
 from datetime import datetime
 import pandas as pd
+import numpy as np
 
 
 class PMData:
 	def __init__(self, Id, type, start_date=datetime(year=2017, month=7, day=1), end_date=datetime.today()):
 		self.Id = Id
 		self.type = type
-		self.pnl = 0
+
 		self.start_date = start_date
 		self.end_date = end_date
 
-	def describe(self, start_date=0, end_date=0):
-		pass
+		self.pnl = pd.DataFrame(columns=['Date', 'Returns'])
+		self.std_pnl = pd.DataFrame(columns=['Date', 'Returns'])
 
 	def set_start_date(self, dt):
 		if type(dt) == datetime:
@@ -25,9 +26,93 @@ class PMData:
 		else:
 			pass
 
+	def cal_describe(self, df=None, start_date=None, end_date=None, item='All'):
+		def cal_stat(df):
+			df = pd.DataFrame(df)
+			r_describe = df['Returns'].describe()
+
+			annualized_std = r_describe['std'] * np.sqrt(250)
+			daily_return = r_describe['mean']
+			annualized_return = r_describe['mean'] / r_describe['count'] * 250
+			sharp = annualized_return / annualized_std
+
+			dict_stat = {"annualized_std": annualized_std,
+			             "daily_return": daily_return,
+			             "annualized_return": annualized_return,
+			             "sharp": sharp}
+			return dict_stat
+
+		def cal_mdd(df):
+			df = pd.DataFrame(df)
+			df['Returns_cumsum'] = df['Returns'].cumsum()
+			df_cum = df[['Date','Returns_cumsum']]
+
+			df_cum['max_here'] = df_cum['Returns_cumsum'].expanding().max()
+			df_cum['dd_here'] = df_cum['max_here'] - df_cum['Returns_cumsum']
+			df_cum.loc[df_cum['dd_here'] == 0, 'max_here_index'] = df_cum.loc[df_cum['dd_here'] == 0, :].index
+			df_cum['max_here_index'] = df_cum['max_here_index'].fillna(method='ffill')
+			df_cum['dd_period'] =  df_cum.index - df_cum['max_here_index']
+
+			series_mdd = df_cum.loc[df_cum['dd_here']==df_cum['dd_here'].max(), :]
+			mdd = float(series_mdd['dd_here'])
+			mdd_date = int(series_mdd['Date']).__str__()
+			mdd_start_date = int(
+				df_cum.loc[series_mdd['max_here_index'], 'Date']).__str__()
+
+			series_lddp = df_cum.loc[df_cum['dd_period']==df_cum['dd_period'].max(), :]
+			lddp = int(series_lddp['dd_period']).__str__()
+			lddp_end_date = int(series_lddp['Date']).__str__()
+			lddp_start_date = int(
+				df_cum.loc[series_lddp.index - series_lddp['dd_period'], 'Date']).__str__()
+
+			dict_mdd = {"mdd":mdd,
+			            "mdd_date":mdd_date,
+			            "mdd_start_date":mdd_start_date,
+			            "lddp":lddp,
+			            "lddp_end_date":lddp_end_date,
+			            "lddp_start_date":lddp_start_date}
+			return dict_mdd
+
+		if df is None:
+			if not (type(self.pnl) == pd.DataFrame):
+				return pd.Series()
+			else:
+				df = pd.DataFrame(self.pnl)
+		else:
+			df = pd.DataFrame(df)
+
+		if start_date is None:
+			start_date = self.start_date.strftime('%Y%m%d')
+		if end_date is None:
+			end_date = self.end_date.strftime('%Y%m%d')
+
+		df = df.loc[df['Date'] <= end_date, :]
+		df = df.loc[df['Date'] >= start_date, :]
+		df = df.reset_index(drop=True)
+
+		dict_stat = {}
+		dict_mdd = {}
+		if item == 'All' or item == 'stat':
+			dict_stat = cal_stat(df)
+		if item == 'All' or item == 'mdd':
+			dict_mdd = cal_mdd(df)
+
+		series_des = pd.Series(dict(dict_stat, **dict_mdd))
+		return series_des
+
+	def pnl_standardize(self):
+		if not(type(self.pnl) == pd.DataFrame):
+			pass
+		else:
+			series_des = self.cal_describe(item='stat')
+			init_annualized_std = float(series_des.annualized_std)
+			mul = 0.12 / init_annualized_std
+			self.std_pnl = pd.DataFrame(self.pnl).copy()
+			self.std_pnl['Returns'] = self.std_pnl['Returns'] * mul
+
 
 class PMProduct(PMData):
-	def __init__(self, Id, user, product_start_at_create=True, strategy_use_last_portfolio=False):
+	def __init__(self, Id, user, product_start_at_create=True, strategy_use_last_portfolio=True):
 		PMData.__init__(self, Id=Id, type='Product')
 		self.portfolio_user_id = user
 		self.list_strategies_id = []
@@ -35,9 +120,7 @@ class PMProduct(PMData):
 
 		self.product_start_at_create = product_start_at_create
 		self.strategy_use_last_portfolio = strategy_use_last_portfolio
-
 		self.list_strategies = []
-		self.pnl = pd.DataFrame()
 
 	def get_product_inf(self, sql_exec):
 		sql = '''
@@ -93,6 +176,8 @@ class PMProduct(PMData):
 			df = pd.DataFrame(pd.concat(l_df))
 			df_r = pd.DataFrame(df.groupby(by='Date')['portfolio_returns'].sum())
 			df_r.rename(columns={'portfolio_returns': 'Returns'}, inplace=True)
+			df_r['Date'] = df_r.index
+			df_r = df_r.reset_index(drop=True)
 			self.pnl = df_r
 
 
@@ -104,9 +189,7 @@ class PMStrategy(PMData):
 		self.portfolio_user_id = portfolio_user_id
 		
 		self.use_last_portfolio = use_last_portfolio
-
 		self.list_traders = []
-		self.pnl = pd.DataFrame(columns=['Date', 'Returns'])
 
 	def get_traders_id(self, sql_exec):
 		sql = '''
@@ -167,6 +250,7 @@ class PMStrategy(PMData):
 			df_r = pd.DataFrame(df.groupby(by='Date')['portfolio_returns'].sum())
 			df_r.rename(columns = {'portfolio_returns': 'Returns'}, inplace=True)
 			df_r['Date'] = df_r.index
+			df_r = df_r.reset_index(drop=True)
 			self.pnl = df_r
 
 
